f27d2a4dd87ef27d9a851a8f98a4a41cf507f7d6
Dennis <deweis@gmx.ch>
2019-06-18 21:43:08 +0200

    Add the plants to the plant list

--- plants.js
+++ plants.js
@@ -1,1 +1,23 @@
-const plants = [];
+const plants = [
+  {
+    name: 'Acker-Rose',
+    latin: 'Rosa agrestis',
+    img: ['rosa_agrestis-wiki.jpg'],
+    url: ['https://de.wikipedia.org/wiki/Acker-Rose'],
+    count: 1
+  },
+  {
+    name: 'Alpen-Rose',
+    latin: 'Rosa pendulina',
+    img: ['rosa_pendulina-wiki.jpg'],
+    url: ['https://de.wikipedia.org/wiki/Gebirgs-Rose'],
+    count: 1
+  },
+  {
+    name: 'Alpenkreuzdorn',
+    latin: 'Rhamnus alpina',
+    img: ['rhamnus_alpina-infoflora.jpg'],
+    url: ['https://www.infoflora.ch/de/flora/rhamnus-alpina.html'],
+    count: 2
+  }
+];
