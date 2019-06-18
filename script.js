e0cc647e3109d5ec5389afc2c873936e55b3ba30
Dennis <deweis@gmx.ch>
2019-06-18 22:05:54 +0200

    Add plants by script to the page

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 function showPlants() {
   const plantsList = document.getElementById('plants-list');
 
-  /* add one card per entry in plants object */
+  /* Add one card per entry in plants object */
+  for (let i = 0; i < plants.length; i++) {
+    const card = document.createElement('div');
+    card.setAttribute('class', 'plant');
+    card.setAttribute('id', generateId(plants[i].latin));
+
+    card.innerHTML = `
+      <div class="card">
+        <a href="${plants[i].url}" target="_blank" rel="noopener">
+          <img
+            src="img/${plants[i].img}"
+            class="card-img-top"
+            alt="${plants[i].latin}"
+          />
+        </a>
+        <div class="card-body">
+          <p class="card-text">
+          ${plants[i].name} (${plants[i].count})
+            <br />
+            <strong>${plants[i].latin}</strong>
+          </p>
+        </div>
+      </div>`;
+
+    plantsList.appendChild(card);
+  }
 }
+
+/* Helper Function to generate an ID based on the plants latin name */
+function generateId(str) {
+  let res = str
+    .toLowerCase()
+    .split(' ')
+    .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+    .join('');
+
+  return res.charAt(0).toLowerCase() + res.substring(1);
+}
+
+showPlants();
